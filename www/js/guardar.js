b2de9fb43f3719a695d3ab4a03149d454a06b65a
estruch18 <estruch95.b@gmail.com>
2015-11-17 00:52:54 +0100

    github

--- www/js/guardar.js
+++ www/js/guardar.js
@@ -1,17 +1,17 @@
 var guardar = {
 
-	var db = "";
-	var nombre = "";
-	var apellidos = "";
-	var cargo = "";
-	var email = "";
-	var edad = "";
-	var telefono = "";
+	db:"",
+	nombre:"",
+	apellidos:"",
+	cargo:"",
+	email:"",
+	edad:"",
+	telefono:"",
+	poblacion:"",
 
 	insertarDatos: function(tx){
 		//INSERCIÓN DE VALORES
-		var sql = "INSERT INTO localDB(nombre, apellidos, cargo, email, edad, telefono) VALUES(
-		'"+nombre+"', '"+apellidos+"', '"+cargo+"', '"+email+"', "+edad+", '"+telefono+"');";
+		var sql = "INSERT INTO localDB VALUES('"+nombre+"', '"+apellidos+"', '"+cargo+"', '"+email+"', "+edad+", '"+telefono+"', '"+poblacion+"');";
 		//EJECUTAMOS LA SENTENCIA SQL
 		tx.executeSql(sql);
 		console.log("ROW INSERT: "+sql);
@@ -22,21 +22,23 @@ var guardar = {
         console.log("MENSAJE DE ERROR "+err.message);
     },
 
-	$("#guardar").click(
+    guardarUsuario: function(){
+    	$("#guardar").click(
 			function(event){
 				console.log("NUEVO ELEMENTO USUARIO");
-				nombre = $("#nombre").val();
-				apellidos = $("#apellidos").val();
-				cargo = $("#cargo").val();
-				email = $("#email").val();
-				edad = $("#edad").val();
-				telefono = $("#telefono").val();
+				this.nombre = $("#nombre").val();
+				this.apellidos = $("#apellidos").val();
+				this.cargo = $("#cargo").val();
+				this.email = $("#email").val();
+				this.edad = $("#edad").val();
+				this.telefono = $("#telefono").val();
+				this.poblacion = $("#poblacion").val();
 
 				//CONEXIÓN CON BBDD
 				this.db = window.openDatabase("localDB", "1.0", "Base de datos de prueba", 2*1024*1024);
 				//TRANSACCIÓN
 				this.db.transaction(this.insertarDatos, this.mostrarErrorGuardar);
 			}
-	);
-
+		);
+    }	
 };
